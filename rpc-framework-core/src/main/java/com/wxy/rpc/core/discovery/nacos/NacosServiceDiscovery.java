736755418d4846847213a8bc7b6a3df0e9d4f608
Mr奇先生 <5608646+CreateQi@user.noreply.gitee.com>
2023-03-17 11:32:55 +0800

    init

---
+++ rpc-framework-core/src/main/java/com/wxy/rpc/core/discovery/nacos/NacosServiceDiscovery.java
@@ -0,0 +1,36 @@
+package com.wxy.rpc.core.discovery.nacos;
+
+import com.wxy.rpc.core.common.RpcRequest;
+import com.wxy.rpc.core.common.ServiceInfo;
+import com.wxy.rpc.core.discovery.ServiceDiscovery;
+import com.wxy.rpc.core.loadbalance.LoadBalance;
+
+import java.io.IOException;
+
+/**
+ * @author CreateQi
+ * @version 1.0
+ * @ClassName NacosServiceDiscovery
+ * @Date 2023/1/8 16:19
+ */
+public class NacosServiceDiscovery implements ServiceDiscovery {
+
+    private final String registryAddr;
+
+    private final LoadBalance loadBalance;
+
+    public NacosServiceDiscovery(String registryAddr, LoadBalance loadBalance) {
+        this.registryAddr = registryAddr;
+        this.loadBalance = loadBalance;
+    }
+
+    @Override
+    public ServiceInfo discover(RpcRequest request) {
+        return null;
+    }
+
+    @Override
+    public void destroy() throws IOException {
+
+    }
+}
